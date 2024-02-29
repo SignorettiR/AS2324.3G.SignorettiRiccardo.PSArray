8f7ccbec8319b8da1bf99e7d9b7a89ade72cca11
SignorettiR <ricky.signoretti@gmail.com>
2024-02-29 08:49:07 +0100

    “feat: implementazione della funzione stampa.voti.pesi e riporto nel main”

--- AS2324.3G.SignorettiRiccardo.PSArray/Program.cs
+++ AS2324.3G.SignorettiRiccardo.PSArray/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace AS2324._3G.SignorettiRiccardo.PSArray;
+﻿using System;
+namespace as2324._3g.Signoretti.Riccardo.Verifica;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-}
 
+
+        Console.WriteLine("Inserisci il numero di voti:");
+        int nVoti = Convert.ToInt32(Console.ReadLine());
+
+        double[] voti = new double[nVoti];
+        int[] pesi = new int[nVoti];
+
+        Random rnd = new Random();
+        for (int i = 0; i < nVoti; i++)
+        {
+            voti[i] = rnd.Next(0, 11);
+            pesi[i] = rnd.Next(0, 101);
+        }
+
+
+
+        StampaVotiPesi(voti, pesi, nVoti);
+        Console.ReadLine();
+
+    }
+
+    static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+    {
+        Console.WriteLine("Voti e Pesi:");
+        for (int i = 0; i < nVoti; i++)
+        {
+            Console.WriteLine($"Voto: {voti[i]}, Peso: {pesi[i]}");
+        }
+    }
+
+}
